73111ee7fc001921f5343a0e427b0b5482547a1c
Litter-Qer <Jonchen399@gmail.com>
2022-05-22 15:10:36 +1000

    Decks

--- blockchain-node-api/contracts/Card.sol
+++ blockchain-node-api/contracts/Card.sol
@@ -9,11 +9,13 @@ contract Card {
     //     hearts,
     //     diamonds
     // }
-    // string public suit;
+
+    uint public suit;
     uint public rank;
 
     // constructor
-    constructor(uint _rank) {
+    constructor(uint _suit, uint _rank) {
+        suit = _suit;
         rank = _rank;
     }
 
@@ -22,16 +24,37 @@ contract Card {
         if ( rank <= 10 ) { return rank; }
         else { return 10; }
     }
+
+    function getSuit() public view returns (uint){
+        return suit;
+    }
 }
 
 /* TODO
     1. complet deck class 
     2. add more test cases
 */
+contract Deck {
+    Card card;
+    Card[] public cards;
 
-// contract Deck is Card {
-//     // constructor
-//     constructor() {
+    // constructor
+    constructor() {
+        for (uint i=1; i<=4; i++){
+            for(uint j=1; j<=13; j++){
+                card = new Card(i,j);
+                cards.push(card);
+            }
+        }
+    }
 
-//     }
-// }
+    function shuffle() public {
+        // Shuffle the cards in a deck
+    }
+
+    function drawOne() public view returns (Card) {
+        // get a card from a card list
+        // TODO !!!! Need to update the deck list **** Not correct at the moment
+        return cards[0];
+    }
+}
--- blockchain-node-api/contracts/Game.sol
+++ blockchain-node-api/contracts/Game.sol
@@ -9,30 +9,40 @@ contract Game {
     mapping (address => uint) internal deposits;
     mapping (address => uint) internal bets;
     mapping (address => bool) internal isStand;
-    // mapping (address => bool) internal isDouble;
+    mapping (address => Card[]) internal hands;
+    mapping (address => bool) internal isDouble;
+
     uint internal lastJoin;
     uint internal playerNum;
     bool internal isStart;
+    bool internal isEnd;
     bool internal debug;
 
+    address public owner;
+    address payable[] public players;
+    bool[] public stands;
+    Card[] hand;
+    
     // debugging events
     event blockNum(uint value);
     event add(address addr);
     event dp(uint value);
     event bt(uint value);
 
-    // Deck deck;
+    Deck deck;
 
     // constructor
-    constructor(address _playerAddress, uint _deposit, uint _bet) {
-        points[_playerAddress] = 0;
-        deposits[_playerAddress] = _deposit;
-        bets[_playerAddress] = _bet;
-        isStand[_playerAddress] = false;
+    // constructor(address _playerAddress, uint _deposit, uint _bet) {
+    constructor() {
+        owner = msg.sender;
+        // points[_playerAddress] = 0;
+        // deposits[_playerAddress] = _deposit;
+        // bets[_playerAddress] = _bet;
+        // isStand[_playerAddress] = false;
         lastJoin = getTime();
         isStart = false;
-        playerNum = 1;
-
+        playerNum = 0;
+        isEnd = false;
         debug= true;
     }
 
@@ -54,6 +64,11 @@ contract Game {
         _;
     }
 
+    modifier GameEnded(){
+        require(isEnd, "Game is not ended");
+        _;
+    }
+
     // **** make sure modify the function to view ****   get time 
     function getTime() private returns(uint time) {
         if (debug) {emit blockNum(block.number); }
@@ -61,9 +76,10 @@ contract Game {
     }
 
     // JoinGame - allows players to join an existing game
-    function joinGame (address addr, uint deposit, uint bet) public gameNotStarted {
-        require(deposit >= 2 * bet && playerNum < 5);    // make sure deposit is greater than bet
-
+    function joinGame (address addr, uint deposit, uint bet) public payable gameNotStarted {
+        require(deposit >= 2 * bet && playerNum <= 5);    // make sure deposit is greater than bet
+        players.push(payable(msg.sender));    // add player to the players (payable)
+        
         if (debug) {
             emit blockNum(block.number); 
             emit add(addr);
@@ -83,13 +99,18 @@ contract Game {
         require(!isStart, "The game has started.");
         require(playerNum > 0, "Not enough players");
         require( getTime() - lastJoin == 3, "Wait until more players join the game.");
-        while (playerNum > 0) 
+
+        deck = new Deck(); 
+        for (uint i=0; i<=playerNum; i++)
         {   
             /* TODO  
             1. get the deck from the contract and random draw two card for each player
             2 .update deck and player points 
             */
-            playerNum --;
+            // hand = [];
+            // hand.push(deck.drawOne());
+            // hand.push(deck.drawOne());
+            // hands[msg.sender] = hand;
         }
 
     }
@@ -101,12 +122,14 @@ contract Game {
             2. need to check all players have standed the game
             3. might need to call withdraw as the game has ended.
         */
+        require(getLength() == playerNum, "Not all players have standed");
+
     }
 
     // stand - allows a player to stand 
     function stand() public gameStarted notStand {
         isStand[msg.sender] = true;
-        // TODO: need to add more attackers resistance cases 
+        stands.push(true);
     }
 
     // hit - allows a player to hit
@@ -115,13 +138,18 @@ contract Game {
             1. randomly draw one card from the deck (consider shuffle the deck)
             2. update player points
         */
+        Card[] storage hand = hands[msg.sender];
+
+
     }
 
     // doubleDown - allows a player to double down
     function doubleDown() public payable gameStarted notStand {
+        require(!isDouble[msg.sender], "You have double downed.");
         require(msg.value >= bets[msg.sender]); // double down ***** ? use == to restrict the value
         hit();
         stand();
+        isDouble[msg.sender] = true;
     }
 
     // reveal - the dealer reveals the card on hand
@@ -139,7 +167,7 @@ contract Game {
     }
 
     // withdraw - dealer or playes can withdraw money 
-    function withdraw() public payable {
+    function withdraw() public payable GameEnded {
         /* TODO
             1. refund money iff something bad happened
             2. allow winners to withdraw their money
@@ -147,6 +175,7 @@ contract Game {
         */
     }
 
+    // Black Jack
     function balckJack() private gameStarted returns (bool isWin) {
         /* TODO
             1. conditions and cards
@@ -154,4 +183,9 @@ contract Game {
             3. return bool
         */
     }
+
+    // get length of a list
+    function getLength() public view returns(uint) {  
+        return stands.length;
+    } 
 }
