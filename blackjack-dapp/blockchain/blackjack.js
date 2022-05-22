3ee37da84e2fbcae266e45029546dfddb9d89287
Jinhao Li <lijinhao828@gmail.com>
2022-05-22 15:22:20 +1000

    'modified'

--- blackjack-dapp/blockchain/blackjack.js
+++ blackjack-dapp/blockchain/blackjack.js
@@ -1,9 +1,10 @@
-const blackjackApi = [{"inputs":[],"name":"count","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"dec","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"get","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"inc","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+const blackjackApi = [{"inputs":[],"stateMutability":"payable","type":"constructor"},{"inputs":[],"name":"deposit","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"notPayable","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address payable","name":"_to","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"transfer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"}];
 
 const blackjackContract = web3 => {
     return new web3.eth.Contract(
         blackjackApi, 
-        '0x0CeeCb07d75dFD3B1d332d44610c5D93cdf4af42'
+        '0x69F7D62C186763fDf747fA56B83c08c7Bf262416'
+        // '0x0CeeCb07d75dFD3B1d332d44610c5D93cdf4af42'
     )
 }
 
--- blackjack-dapp/pages/index.js
+++ blackjack-dapp/pages/index.js
@@ -10,33 +10,49 @@ export default function Home() {
   const [web3, setWeb3] = useState();
   const [account, setAccount] = useState();
   const [contract, setContract] = useState();
-  const [count, setCount] = useState();
+  const [balance, setBalance] = useState(0);
   
   useEffect(() => {
     // initialize count once web3 contract is loaded
-    if (contract) updateCount()
+    async function fetchData() {
+      if (contract) {
+        await getBalance();
+      }
+    }
+    fetchData();
   }, [contract]); 
 
-  const updateCount = async () => {
-    const newCount = await contract.methods.get().call();
-    setCount(newCount);
+  const getBalance = async () => {
+    const bal = await web3.eth.getBalance(account);
+    setBalance(web3.utils.fromWei(bal, 'ether'));
   }
 
-  const increase = async () => {
-    console.log("increase 1");
-    await contract.methods.inc().send({ from: account });
-    await updateCount();
+  const pay = async () => {
+    const tx = await contract.methods.deposit().send(
+      { 
+        from: account,
+        value: '15000000000000000',
+        gas: 300000,
+        gasPrice: null
+      }
+    );
+    console.log(tx);
+    await getBalance();
   }
 
-  const decrease = async () => {
-    console.log('decrease 1');
-    await contract.methods.dec().send({ from: account });
-    await updateCount();
-  };
+  const withdraw = async () => {
+    await contract.methods.withdraw().send({
+      from: account,
+      gas: 300000,
+      gasPrice: null
+    })
+    console.log(await web3.eth.getBalance(contract.options.address));
+    await getBalance();
+  }
 
   const connectWalletHandler = async () => {
     if (window.ethereum) {
-      await window.ethereum.enable();
+      // await window.ethereum.enable();
       
       const provider = new Web3.providers.HttpProvider('http://localhost:7545');
       var web3 = new Web3(provider);
@@ -44,6 +60,7 @@ export default function Home() {
       
       // setup the account with the first one
       const accounts = await web3.eth.getAccounts();
+      // console.log(accounts)
       setAccount(accounts[0]); 
 
       // create local contract instance
@@ -62,33 +79,36 @@ export default function Home() {
       </Head>
 
       <main>
-        <div className="row">
-          <div className="col-md-12">
+        <div className="row mt-3">
+          <div className="col-3">
             <h1>Blackjack</h1>
             <p>An Ethereum Blackjack Dapp</p>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <p>Count: {count}</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
+          <div className="col-3">
             <button className="btn btn-primary" onClick={connectWalletHandler}>
               Connect Wallet
             </button>
           </div>
         </div>
-        <div className="row mt-2">
-          <div className="col-1">
-            <button className="btn btn-primary" onClick={increase}>
-              +
+        <div className="row">
+          <div className="col-12">
+            <h4>My Address: {account}</h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h4>My Balance: {balance} Ether</h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+            <button className="btn btn-primary" onClick={pay}>
+              Pay
             </button>
           </div>
-          <div className="col-1">
-            <button className="btn btn-primary" onClick={decrease}>
-              -
+          <div className="col-3">
+            <button className="btn btn-primary" onClick={withdraw}>
+              Withdraw
             </button>
           </div>
         </div>
